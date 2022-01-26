fc9175114642446de67b23bee82d737bd70a0e81
bryan kretz <kretzbryan@gmail.com>
2022-01-26 00:15:04 -0800

    initial skeleton code

---
+++ models/index.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const db = 'mongodb://localhost:27017';
+
+const connectDB = async () => {
+	try {
+		await mongoose.connect(process.env.MONGO_URI || db, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useFindAndModify: false,
+			useCreateIndex: true,
+		});
+
+		console.log('db connected!!');
+	} catch (err) {
+		console.log(err.message);
+
+		process.exit(1);
+	}
+};
+
+module.exports = {
+	connectDB: connectDB,
+};
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const controllers = require('./controllers');
+
+require('dotenv').config();
+
+// Add MONGO_URI and PORT  in .env file to get started!
+// Uncomment code below
+
+const db = require('./models');
+const connectDB = db.connectDB;
+
+connectDB();
+
+const PORT = process.env.PORT;
+
+app.get('/', (req, res) => {
+	res.send('API Running');
+});
+
+app.use('/api/landing', controllers.landing);
+
+app.listen(PORT, () => {
+	console.log(`Server on port ${PORT}`);
+});
